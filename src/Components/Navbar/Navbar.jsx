a07055421f332e4636b454482dc8931aa92e09cc
Vinayak Puranik <vinayakpuranik0707@gmail.com>
2024-09-01 23:59:57 +0530

    File sturucture updated

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import style from './styles/Navbar.module.css'
+
+const Navbar = () => {
+  return (
+    <>
+        <div className={`${style.navBox}`}>
+            HELLO
+        </div>
+    </>
+  )
+}
+
+export default Navbar
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import Navbar from '../../Components/Navbar/Navbar'
+
+const Home = () => {
+  return (
+    <>
+        <Navbar/>
+    </>
+  )
+}
+
+export default Home
