dc5967289315b0faaf8dc69644cb061b747ea854
Vinayak Puranik <vinayakpuranik0707@gmail.com>
2024-10-11 23:12:42 +0530

    File structure updated and minor fixes

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -2,13 +2,17 @@
 import React from 'react'
 import styles from './Profile.module.css'
 import ProfileCard from '../../Components/ProfileCard/ProfileCard'
+import Navbar from '../../Components/Navbar/Navbar'
+import Footer from '../../Components/Footer/Footer'
 
 function Profile() {
   return (
     <div>
+        <Navbar/>
+        
         <ProfileCard/>
 
-        
+        <Footer/>
     </div>
   )
 }
